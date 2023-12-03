2665cb7e06f646d31d76b423587a0d345b856996
notaiax <aiax.developer@gmail.com>
2023-12-03 13:41:40 +0100

    modify job script

--- VAE/execute_VAE.sh
+++ VAE/execute_VAE.sh
@@ -1,34 +1,36 @@
-#!/bin/sh 
-### General options 
-### -- specify queue -- 
-#BSUB -q hpc
-### -- set the job Name -- 
-#BSUB -J example
-### -- ask for number of cores (default: 1) -- 
-#BSUB -n 4 
-### -- specify that the cores must be on the same host -- 
-#BSUB -R "span[hosts=1]"
-### -- specify that we need 4GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=4GB]"
-### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
-#BSUB -M 5GB
-### -- set walltime limit: hh:mm -- 
-#BSUB -W 24:00 
-### -- set the email address -- 
+#!/bin/sh
+### General options
+### –- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J testjob
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 1:00
+# request 5GB of system-memory
+#BSUB -R "rusage[mem=5GB]"
+### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
-#BSUB -u s223237@student.dtu.dk
-### -- send notification at start -- 
-#BSUB -B 
-### -- send notification at completion -- 
-#BSUB -N 
-### -- Specify the output and error file. %J is the job-id -- 
-### -- -o and -e mean append, -oo and -eo mean overwrite -- 
-#BSUB -o execute.out 
-#BSUB -e execute.err 
+#BSUB -u s222703@dtu.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu_%J.out
+#BSUB -e gpu_%J.err
+# -- end of LSF options --
 
-# here follow the commands you want to execute with input.in as the input file
-# bsub < pca.sh
-# bstat
+#BSUB -R "select[gpu32gb]"
+
+nvidia-smi
+# Load the cuda module
 source ../venv_iso/bin/activate
+module swap cuda/12.2.2
+
 python aiax_VAE.py
