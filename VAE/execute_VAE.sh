77fb4c66f12f9ce3eb05ea6883325f15874a58b2
notaiax <aiax.developer@gmail.com>
2023-12-03 13:22:34 +0100

    Add sumbit job code

---
+++ VAE/execute_VAE.sh
@@ -0,0 +1,34 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q hpc
+### -- set the job Name -- 
+#BSUB -J example
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4 
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=4GB]"
+### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
+#BSUB -M 5GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s223237@student.dtu.dk
+### -- send notification at start -- 
+#BSUB -B 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o execute.out 
+#BSUB -e execute.err 
+
+# here follow the commands you want to execute with input.in as the input file
+# bsub < pca.sh
+# bstat
+source ../venv_iso/bin/activate
+python aiax_VAE.py
